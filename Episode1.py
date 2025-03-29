820068ebced5e2ed36817f410785357f266d4267
Vivek <hinguvivek05@gmail.com>
2025-03-30 00:10:08 +0530

    String basics

--- Episode1.py
+++ Episode1.py
@@ -1,9 +1,6 @@
-a = "Vivek"
-print(a.endswith("ek")) # true
-print(a.startswith("Vi")) #true # if you declare in small letter so it is shows thats False
-print(a.capitalize()) # only capital first character # V
-print(a.upper()) # uppercase all character
-print(a.strip()) # reduce the space like # "  vivek   " -> "vivek"
-print(a.replace("ivek","enkatesh")) # venkatesh
-b= "Vivek is ai engineer \n its intrust in designning \t but not a \"job in designing\"" # o/p "job in designing"
-print(b)
+Name =input("whats your name:")
+print(f"Good afternoon {Name}!!!") # good afternoon vir!!!
+
+a = " Hingu  Vivek   Alpesh   kumar"
+print(a.find("  ")) # 6 (double space)
+print(a.replace("  "," ")) # convert double space to single 
