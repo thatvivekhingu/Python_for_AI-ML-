8a529eb0b4ef8e0d51c2daa0a6661a7fd5b51780
Vivek <hinguvivek05@gmail.com>
2025-03-29 23:06:06 +0530

    any other basic method of string

--- Episode1.py
+++ Episode1.py
@@ -1,6 +1,10 @@
 #short the name 
-Name="Vivekkumar"
+Name="abcdefghijklmnopqrstuvwxyz"
 nameshort=Name[0:3] #start with 0 and ends with 2 (excluding 3)
-print(nameshort)
+name=Name[1]
+print(nameshort) #o/p abc
+print(name) #o/p b
+print(len(Name)) #o/p 26
 
-#o/p: Viv
+b =Name[1:9:2] # contain 1 to 9 then first letter contain and take jump of 2 and conatins char
+print(b) # bdfh
