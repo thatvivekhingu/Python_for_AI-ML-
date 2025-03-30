302f5c91d2a4a107607e9e365a0985522dda956c
Vivek <hinguvivek05@gmail.com>
2025-03-30 12:31:06 +0530

    list and tuple

--- Episode1.py
+++ Episode1.py
@@ -1,6 +1,25 @@
-Name =input("whats your name:")
-print(f"Good afternoon {Name}!!!") # good afternoon vir!!!
+# List in python # list is a mutable 
+l1=["vivek","yash",349.0,555]
+l1.append("Hingu")
+print(l1) # add hingu at the end of list
 
-a = " Hingu  Vivek   Alpesh   kumar"
-print(a.find("  ")) # 6 (double space)
-print(a.replace("  "," ")) # convert double space to single 
+l10=[44,67,35,1,99,87]
+l10.sort()
+print(l10) #list is arrange in asending order
+l10.reverse()
+print(l10)#reverse our list is print
+l10.pop(3)
+print(l10) # 3rd number liste iteam will be pop 
+l10.insert(3,786)
+print(l10)# add number of in list at the 3rd place and entered number
+
+
+
+# Tupple in Python # tuple is immutable
+
+a=(189,25,23,"vivek","guru",23,23,23)
+print(a)
+b = a.count(23) # how many numbers are their in this list declare number of 23
+print(b)
+c = a.index(23) # which place number 23 is located
+print(c)
