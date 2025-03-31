d7914e0c4f778ec4198076e44b29d78ec550c4df
Vivek <hinguvivek05@gmail.com>
2025-03-31 20:22:21 +0530

    sets in python

--- Episode1.py
+++ Episode1.py
@@ -1,14 +1,15 @@
-#dictionary in python chpater 5 #
-#  It is mutable
-# it is a key and value pairs
-marks={"vivek":79,
-       "yash":87,
-       "Vaibhavi":88
-    }
+# sets in python
+# set is a collection of non-repititive elements
 
-print(marks.type(marks))
-print(marks["vivek"]) # define pair of element first  so they give second element of the pair 
-print(marks.items()) # tey shows that all iteam which is contains in dict in tuple form
-print(marks.keys) # show all the key
-print(marks.values) #show all the values
-print(marks.update({"vivek":99,"vaibhavi ":99,"sakshi":88})) # it is updates any values of pair and add pair in dict
+s = {2,3,4,4,4,5,6,6,6,"vivek","vivek"}
+print(type(s))
+print(s) # {2,3,4,5,6,vivek}
+s.add(22)# contains only one arguments 
+print(s)
+s.remove("vivek") # remove from the set any elements
+print(s)
+
+s1={"genix","gemini","vivek",2}
+print(s.union(s1)) #{2,3,4,5,6,'vivek',22,'genix','gemini'}
+print(s.intersection(s1))  #{2} #only output have numbers
+
