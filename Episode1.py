cf3f7410ac4b75682667c08f4d6a1a808424f51a
Vivek <hinguvivek05@gmail.com>
2025-04-03 19:26:44 +0530

    recursion in python

--- Episode1.py
+++ Episode1.py
@@ -1,43 +1,9 @@
-# functions in python
-
-'''def sum():
-    a=int(input("enter number"))
-    b=int(input("enter number"))
-
-    c=a+b
-    print(c)
-
-sum()
-'''
-# function with arguments
-
-def say(name):
-    print("good morning"+ " "+name)
-say("vivek")
-
-
-def say(name,ending):
-    print("good morning"+ " "+name +" "+ending)
-say("vivek","how do you do??")
-
-
-# in function with return value
-
-def say():
-    print("good morning")
-    return "ok"
-a=say()
-print(a)
-# output 
-# good morning
-# ok
-
-
-
-#functions in default arguments
-
-def say(name,ending="kem chho?"):
-    print("good morning"+ " "+name+" "+ending)
-say("vivek" ,"majama" ) # o/p good morning vivek majama
-say ("vivek") # o/p good morning vivek kem chho
-
+# recursion in python
+def factorial(n):
+    if(n==1 or n==0):
+        return 1
+    return n * factorial(n-1)
+        
+    
+n= int(input("enter the number:"))
+print(factorial(n))
