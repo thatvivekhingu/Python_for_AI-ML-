2af1e2c685e458e5cdc2da9458858e1e2b11c05d
Vivek <hinguvivek05@gmail.com>
2025-04-03 19:43:05 +0530

    question of practice

--- Episode1.py
+++ Episode1.py
@@ -1,9 +1,25 @@
-# recursion in python
-def factorial(n):
-    if(n==1 or n==0):
-        return 1
-    return n * factorial(n-1)
-        
+# practice que chpter 8
+
+# que :Write a program using functions to find greatest of three numbers.
+
+'''
+def great(a,b,c):
     
-n= int(input("enter the number:"))
-print(factorial(n))
+    
+    if(a>b and a>c):
+        print(a,"is greatest number:")
+    elif(b>a and b>c):
+        print(b,"is greatest number:")
+    elif(c>b and c>a):
+        print(c,"is greatest number:")
+
+great(4,7,3)'''
+
+# que  : Write a python program using function to convert Celsius to Fahrenheit.
+
+def converter():
+    c=int(input("enter celcious :"))
+    f=(c*1.8)+32
+    print(f"Fehrenhit value is :{f}")
+    
+converter()
