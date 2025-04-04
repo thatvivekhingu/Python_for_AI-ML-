b3b56f280eccabdff305f1b10466d7c561e1c502
Vivek <hinguvivek05@gmail.com>
2025-04-04 19:11:23 +0530

    Files ip/op in python

--- Episode1.py
+++ Episode1.py
@@ -1,25 +1,24 @@
-# practice que chpter 8
+# file input/output in python
 
-# que :Write a program using functions to find greatest of three numbers.
+# first create  one file name is myfixe. txt then this file write content : hello genix what are you doing now??( not derived ,simply write )
+# reading mode
+f=open("myfile.txt")
+data =f.read()
+print(data)
+f.close()
 
-'''
-def great(a,b,c):
-    
-    
-    if(a>b and a>c):
-        print(a,"is greatest number:")
-    elif(b>a and b>c):
-        print(b,"is greatest number:")
-    elif(c>b and c>a):
-        print(c,"is greatest number:")
 
-great(4,7,3)'''
 
-# que  : Write a python program using function to convert Celsius to Fahrenheit.
+# when file not create first anfter code run then create automaticaly in file in vs code
+# this is contain write mode
+line="hello genix what are you doing now??"
+f=open("myfile","w")
+f.write(line)
+f.close()
 
-def converter():
-    c=int(input("enter celcious :"))
-    f=(c*1.8)+32
-    print(f"Fehrenhit value is :{f}")
-    
-converter()
+
+# if you don't write f.close()so you try:
+# with statement 
+
+with open("myfile.txt") as f:
+    print(f.read())
