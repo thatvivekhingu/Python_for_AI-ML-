a1aff79a9ae86dd37c0882284f1fea6071424d8f
Vivek <hinguvivek05@gmail.com>
2025-04-07 19:52:57 +0530

    oop : create class and object

--- Episode1.py
+++ Episode1.py
@@ -1,24 +1,10 @@
-# file input/output in python
-
-# first create  one file name is myfixe. txt then this file write content : hello genix what are you doing now??( not derived ,simply write )
-# reading mode
-f=open("myfile.txt")
-data =f.read()
-print(data)
-f.close()
+# Object oreinted programming in python 
 
 
+class vivek():          # create thge class
+    langauage="python"
+    job="Bnaglore"
+    salary="30,000"
 
-# when file not create first anfter code run then create automaticaly in file in vs code
-# this is contain write mode
-line="hello genix what are you doing now??"
-f=open("myfile","w")
-f.write(line)
-f.close()
-
-
-# if you don't write f.close()so you try:
-# with statement 
-
-with open("myfile.txt") as f:
-    print(f.read())
+It=vivek   # create object "It"
+print(It.langauage,It.salary,It.job)
