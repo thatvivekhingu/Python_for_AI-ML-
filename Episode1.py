052037dd950ce4bfb72256f68cd79c4a98daa388
Vivek <hinguvivek05@gmail.com>
2025-04-09 19:17:00 +0530

    Super key word

--- Episode1.py
+++ Episode1.py
@@ -1,13 +1,25 @@
-# MUltilevel Inheritance in python
+# super key
 
 class grandfather:
+    def __init__(self):  # create the constuructor
+        print("constructor of grandfather")
     a="Dada"
 
 class father(grandfather):
-    b ="Pappa"
+     def __init__(self):
+        print("constructor of father")
+     b ="Pappa"
 
 class Son(father):
-    c="Son"
+     def __init__(self):
+        super().__init__()            # used super key to also call its method and its super class method
+        print("constructor of son")
+     c="Son"
 
 obj=Son()
-print(obj.c,obj.b,obj.a)
+print(obj.c)
+
+#o/p: 
+# constructor of father 
+# constructor of Son 
+# Son
