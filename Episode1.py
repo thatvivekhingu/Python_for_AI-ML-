113287497027710dda4a7aeb47b776f3b6794446
Vivek <hinguvivek05@gmail.com>
2025-04-09 19:32:06 +0530

    @class method decorator

--- Episode1.py
+++ Episode1.py
@@ -1,25 +1,35 @@
-# super key
+# @class method decorator
 
-class grandfather:
-    def __init__(self):  # create the constuructor
-        print("constructor of grandfather")
-    a="Dada"
+class body():
+    def show(self):
+        print(f"The upadates value is print in self : {self.a}")
+    a=45
+  
+    
+obj=body()
+obj.a=69
+obj.show()
 
-class father(grandfather):
-     def __init__(self):
-        print("constructor of father")
-     b ="Pappa"
+#o/p 
+# The upadates value is print in self :69
 
-class Son(father):
-     def __init__(self):
-        super().__init__()            # used super key to also call its method and its super class method
-        print("constructor of son")
-     c="Son"
+    
 
-obj=Son()
-print(obj.c)
+    # this portion self is also contains the instance value but you want to take only class value so you define
+    # @classmethod
 
-#o/p: 
-# constructor of father 
-# constructor of Son 
-# Son
+class deco():
+    
+
+    @classmethod
+    def show(cls):
+        print(f"The decorator value is print in class is : {cls.a}")
+    a=45
+    
+obj2=deco()
+obj2.a=99
+obj2.show()
+
+#o/p:
+#The decorator value is print in class is :45
+
