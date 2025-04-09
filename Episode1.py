d4beb605bfbc7df032a6ef1265d430009f92574d
Vivek <hinguvivek05@gmail.com>
2025-04-09 09:10:01 +0530

    Multilevel inheritance

--- Episode1.py
+++ Episode1.py
@@ -1,23 +1,13 @@
-# MUltiple Inheritance in python
+# MUltilevel Inheritance in python
 
-class First:
-    name="Vivek"
-    skills="AI and ML"
-    def job(self):
-        print(f"Employee name is {self.name} and his skills in {self.skills}")
+class grandfather:
+    a="Dada"
 
-class Second:
-    Language="Python"
-    Hobby="Coding"
-    def Expierence(self):
-        print(f"Language is {self.Language} and his hobby is  {self.Hobby}")
+class father(grandfather):
+    b ="Pappa"
 
-class main(First,Second):
-    def CV(self):
-        print(f"Name of employee is {self.name} and its hobby is {self.Hobby}")
+class Son(father):
+    c="Son"
 
-
-obj=main()
-obj.CV()
-obj.job()
-obj.Expierence()
+obj=Son()
+print(obj.c,obj.b,obj.a)
