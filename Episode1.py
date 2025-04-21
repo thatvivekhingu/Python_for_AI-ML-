44f2dbc18e8d6052824ae6406d57741bec7ed73e
Vivek <hinguvivek05@gmail.com>
2025-04-21 08:46:15 +0530

    raise in py

--- Episode1.py
+++ Episode1.py
@@ -1,13 +1,17 @@
-# exception 
+# raise in prg( agar aapka dev prg me galati  kar rha hai toh uske code ko stop kardo prg stop kardo)
 
-try:
-    a=int(input("hey, Enter thr number : "))
-    print(a)
-except Exception as e:
-    print("invalid number enter a valid number")
+a=int(input("enter number :"))
+b=int(input("enter second number:"))
 
-# agar program me input string type ki leli toh vo exception me jayega error msg show nahi hoga
+if(b==0):
+    raise ZeroDivisionError("zero division is not allowed")
 
+print(f"the division of {a} and {b} is :{a/b}")
 
-        
-        
+#output
+'''enter number :3
+enter second number:0
+Traceback (most recent call last):
+  File "c:\Users\Vivek Hingu\OneDrive\Desktop\moviesSeason01\Episode1.py", line 7, in <module>
+    raise ZeroDivisionError("zero division is not allowed")
+ZeroDivisionError: zero division is not allowed'''
