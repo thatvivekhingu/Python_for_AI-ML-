448241579d31f592737214ce07c55c22b1df7819
Vivek Hingu <hinguvivek05@gmail.com>
2025-08-08 21:22:31 +0530

    Delete musicLibrary.py

--- musicLibrary.py
+++
@@ -1,8 +0,0 @@
-music = {
-    "hello" :"https://youtube.com/shorts/QC8OLDw9AQE?si=Jcmh1Nl1-ArI7l1C",
-    "wow" : "https://youtu.be/iAIBF2ngbWY?si=Jz5kZHMRo-vmxQ3b",
-    "happy": "https://youtu.be/WVMSjMtPbq0?si=tg81RhRyRUiYrN_1",
-    "sad": "https://youtu.be/OdrOp7JwZiE?si=r4mvuvNYyBwAltfs"
-}
-
-
